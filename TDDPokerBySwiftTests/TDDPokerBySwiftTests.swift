f68016f28ca92e83ab11b531bfc9f0b49a05f1f7
cm-TanakaKenji <tanaka.kenji@classmethod.jp>
2018-04-18 01:53:30 +0900

    hasSameRankの失敗するテストを記述

--- TDDPokerBySwiftTests/TDDPokerBySwiftTests.swift
+++ TDDPokerBySwiftTests/TDDPokerBySwiftTests.swift
@@ -32,4 +32,13 @@ class TDDPokerBySwiftTests: XCTestCase {
         card2 = Card(rank: .two, suit: .heart)
         XCTAssertFalse(card1.hasSameSuit(card2))
     }
+    
+    func testHasSameRank() {
+        var card1: Card
+        var card2: Card
+        
+        card1 = Card(rank: .two, suit: .spade)
+        card2 = Card(rank: .two, suit: .heart)
+        XCTAssertTrue(card1.hasSameRank(card2))
+    }
 }
