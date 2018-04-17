481f314bbb98de2ddea1a9ce887e4630f90b6674
cm-TanakaKenji <tanaka.kenji@classmethod.jp>
2018-04-18 00:10:48 +0900

    テストケースを追加することで、Cardの生成が特定のパタンでのみ行われるわけではないことを証明する

--- TDDPokerBySwift/Card.swift
+++ TDDPokerBySwift/Card.swift
@@ -10,11 +10,26 @@ import Foundation
 
 struct Card {
     enum Suit {
+        case spade
         case heart
+        case club
+        case diamond
     }
     
     enum Rank {
+        case ace
+        case two
         case three
+        case four
+        case five
+        case six
+        case seven
+        case eight
+        case nine
+        case ten
+        case jack
+        case queen
+        case king
     }
     
     let suit: Suit
