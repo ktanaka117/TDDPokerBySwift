ce7e975c37f880196ca89c982674bbec699d7bed
cm-TanakaKenji <tanaka.kenji@classmethod.jp>
2018-04-18 01:01:55 +0900

    ついでにリファクタリング

--- TDDPokerBySwiftTests/TDDPokerBySwiftTests.swift
+++ TDDPokerBySwiftTests/TDDPokerBySwiftTests.swift
@@ -23,10 +23,12 @@ class TDDPokerBySwiftTests: XCTestCase {
     }
     
     func testCardNotation() {
-        let card1 = Card(suit: .heart, rank: .three)
-        XCTAssertEqual(card1.notation, "3♥")
+        var card: Card
         
-        let card2 = Card(suit: .spade, rank: .jack)
-        XCTAssertEqual(card2.notation, "J♠")
+        card = Card(suit: .heart, rank: .three)
+        XCTAssertEqual(card.notation, "3♥")
+        
+        card = Card(suit: .spade, rank: .jack)
+        XCTAssertEqual(card.notation, "J♠")
     }
 }
