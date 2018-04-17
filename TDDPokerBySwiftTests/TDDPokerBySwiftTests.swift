34e020c438934342f7a7a02098c88e514c74e64d
cm-TanakaKenji <tanaka.kenji@classmethod.jp>
2018-04-18 00:07:04 +0900

    レッドから始めるためにテストを記述

--- TDDPokerBySwiftTests/TDDPokerBySwiftTests.swift
+++ TDDPokerBySwiftTests/TDDPokerBySwiftTests.swift
@@ -10,27 +10,9 @@ import XCTest
 @testable import TDDPokerBySwift
 
 class TDDPokerBySwiftTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testInitializeCard() {
+        let card = Card(suit: .heart, rank: .three)
+        XCTAssertEqual(card.suit, .heart)
+        XCTAssertEqual(card.rank, .three)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
