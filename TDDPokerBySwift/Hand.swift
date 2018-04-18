1ccb78b6e3212ec68a515a0e5e274c64c379c5a1
cm-TanakaKenji <tanaka.kenji@classmethod.jp>
2018-04-18 09:56:05 +0900

    ペアの判定処理を仮実装

---
+++ TDDPokerBySwift/Hand.swift
@@ -0,0 +1,17 @@
+//
+//  Hand.swift
+//  TDDPokerBySwift
+//
+//  Created by tanaka.kenji on 2018/04/18.
+//  Copyright © 2018年 tanaka.kenji. All rights reserved.
+//
+
+import Foundation
+
+struct Hand {
+    let cards: [Card]
+    
+    var isPair: Bool {
+        return true
+    }
+}
